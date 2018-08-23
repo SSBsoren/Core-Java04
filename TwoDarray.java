7ded95b5ef6663487f50b69ef14196c3c3763c45
saGen <sagensakam015@gmail.com>
2018-08-23 23:45:24 +0530

    To determine the upper bound of two dimentional array.

---
+++ TwoDarray.java
@@ -0,0 +1,11 @@
+//To determine the upper bound of two dimentional array.
+package Core_java04;
+
+public class TwoDarray {
+    public static void main(String args[])
+    {
+        String[][] data=new String[2][5];
+        System.out.println("Dimension 1 : "+data.length);
+        System.out.println("Dimension 2 : "+data[0].length);
+    }
+}
